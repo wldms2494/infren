7a59fb91d167d3380c5ee291f69d057946c6302d
wldms2494 <wldms2494@gmail.com>
2022-01-09 16:48:28 +0900

    구체타입으로 조회

--- src/test/java/beanfind/ApplicationContextExtendsFindTest.java
+++ src/test/java/beanfind/ApplicationContextExtendsFindTest.java
@@ -7,7 +7,6 @@ import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FIxDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import java.util.Map;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
@@ -22,41 +21,47 @@ public class ApplicationContextExtendsFindTest {
 
   @Test
   @DisplayName("부모 출력하면 자식들도 나오냐")
-  //내가 먼저 작성해본 테스트
-  void findBeanByParentsClass(){
-    Map<String, DiscountPolicy> beansOfType = ac.getBeansOfType(DiscountPolicy.class);
-    for(String key : beansOfType.keySet()){
-      System.out.println("key - " + key );
-    }
+	//내가 먼저 작성해본 테스트
+  void findBeanByParentsClass() {
+	Map<String, DiscountPolicy> beansOfType = ac.getBeansOfType(DiscountPolicy.class);
+	for (String key : beansOfType.keySet()) {
+	  System.out.println("key - " + key);
+	}
   }
 
   @Test
   @DisplayName("부모 타입으로 조회시 , 자식이 둘이상있으면 noUniqueException발생?")
-  void findBeanByParentTypeDuplicate(){
+  void findBeanByParentTypeDuplicate() {
 //    DiscountPolicy bean = ac.getBean(DiscountPolicy.class);
-   assertThrows(NoUniqueBeanDefinitionException.class,
-        ()-> ac.getBean(DiscountPolicy.class));
+	assertThrows(NoUniqueBeanDefinitionException.class,
+		() -> ac.getBean(DiscountPolicy.class));
   }
 
   @Test
   @DisplayName("부모 타입으로 조회시 , 자식이 둘이상있으면 빈 이름을 지정하면 된다")
-  void findBeanByParentTypeBeanName(){
-    DiscountPolicy rateDiscountPolicy = ac.getBean("rateDiscountPolicy", DiscountPolicy.class);
-    assertThat(rateDiscountPolicy).isInstanceOf(RateDiscountPolicy.class);
+  void findBeanByParentTypeBeanName() {
+	DiscountPolicy rateDiscountPolicy = ac.getBean("rateDiscountPolicy", DiscountPolicy.class);
+	assertThat(rateDiscountPolicy).isInstanceOf(RateDiscountPolicy.class);
   }
 
-
+  @Test
+  @DisplayName("특정 하위 타입으로 조회")
+  void findBeanBySubType(){
+    RateDiscountPolicy bean = ac.getBean(RateDiscountPolicy.class);
+    assertThat(bean).isInstanceOf(RateDiscountPolicy.class);
+  }
 
   @Configuration
   static class TestConfig {
-    @Bean
-    public DiscountPolicy rateDiscountPolicy(){
-      return new RateDiscountPolicy();
-    }
 
-    @Bean
-    public DiscountPolicy fixDiscountPolicy(){
-      return new FIxDiscountPolicy();
-    }
+	@Bean
+	public DiscountPolicy rateDiscountPolicy() {
+	  return new RateDiscountPolicy();
+	}
+
+	@Bean
+	public DiscountPolicy fixDiscountPolicy() {
+	  return new FIxDiscountPolicy();
+	}
   }
 }
